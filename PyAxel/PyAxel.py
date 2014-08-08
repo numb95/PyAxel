583bbe1e13f1c74e85a72161da6bc9320b30e72c
mehdy <mehdy.khoshnoody@gmail.com>
2014-08-08 15:51:22 +0430

    fixed bugs for " ' " in urls completed downloading form list and updating list also for multi url downloading

--- PyAxel/PyAxel.py
+++ PyAxel/PyAxel.py
@@ -29,22 +29,24 @@ def version(ctx, param, value):
 @click.command()
 @click.option('-a', '--alternate', is_flag=True, help="Alternate progress indicator")
 @click.option('-H', '--header', type=click.STRING, help="Add heaser string")
-@click.option('-l', '--from-list', type=click.File('rw'), multiple=True,
+@click.option('-l', '--from-list', type=click.File('rw+'), multiple=True,
               help="Download a list of links")  # new added option
 @click.option('-n', '--num-connections', type=int, help="Specify maximum number of connections")
 @click.option('-N', '--no-proxy', is_flag=True, help="Just don't use any proxy server")
 @click.option('-o', '--output', help="Specify local output file")  # TODO: type=click.Path
+@click.option('-p', '--password', required=False, help="HTTP password")
 @click.option('-q', '--quiet', is_flag=True, help="Leave stdout alone")
 @click.option('-s', '--max-speed', type=int, help="Specify maximum speed (bytes per second)")
-@click.option('-S', '--search', type=str,
+@click.option('-S', '--search', type=click.STRING,
               help="Search for mirrors and download from x servers")  # TODO: not sure about type
+@click.option('-u', '--username', type=click.STRING, required=False, help="HTTP username")
 @click.option('-U', '--user-agent', type=str, help="Set user agent")  # TODO: not sure about type
 @click.option('-v', '--verbose', is_flag=True, help="More status information")
 @click.option('-V', '--version', is_flag=True, callback=version, expose_value=False, is_eager=True,
               help="Version information")
-@click.argument('url', required=False)  # TODO: multiple=True,
+@click.argument('url', required=False, nargs=-1)
 def generate(alternate, header, from_list, num_connections, no_proxy, output, quiet, max_speed, search, user_agent,
-             verbose, url):
+             verbose, url, username, password):
     """
     An Improvement of axel download accelerator.
     """
@@ -60,18 +62,25 @@ def generate(alternate, header, from_list, num_connections, no_proxy, output, qu
     if user_agent: cmd += '-U ' + user_agent + ' '
     if verbose: cmd += '-v '
     if url:
-        # for item in url:
-        if not os.path.isfile(urllib2.unquote(url.split('/')[-1])):
-            cmd += url + ' '
-            click.echo('=====================================================')
-            click.echo(url)
-            click.echo('=====================================================')
-            execute(cmd)
-        else:
-            click.echo(urllib2.unquote(url.split('/')[-1]))
-            click.echo('The file "%s" exists' % unicode(urllib2.unquote(url.split('/')[-1])) +
-                       ' in the downloading directory.\nif you are sure this is something else,' +
-                       ' please download it to somewrhere else or move the existing file')
+        for item in url:
+            if username and password:
+                item = item.partition('://')
+                item.insert(2, username + ':' + password + '@')
+                item = ''.join(item)
+            elif username or password:
+                click.echo('Please enter both username and password if you wish do authentication for your requests')
+            if not os.path.isfile(urllib2.unquote(item.split('/')[-1])) or os.path.isfile(
+                            urllib2.unquote(item.split('/')[-1]) + '.st'):
+                click.echo('======================================================================================')
+                click.echo(item)
+                click.echo('======================================================================================')
+                execute(cmd + '"' + item + '"')
+            else:
+                click.echo('======================================================================================')
+                click.echo('The file "%s" exists' % unicode(urllib2.unquote(item.split('/')[-1])) +
+                           ' in the downloading directory.\nif you are sure this is something else,' +
+                           ' please download it to somewhere else or move the existing file')
+                click.echo('======================================================================================')
     # Download for a list of urls
     if from_list:
         for item in from_list:
@@ -79,21 +88,32 @@ def generate(alternate, header, from_list, num_connections, no_proxy, output, qu
             file_list = file_generic.read().split('\n')
             write_list = list(file_list)
             for url in file_list:
-                if not os.path.isfile(urllib2.unquote(url.split('/')[-1])):
-                    cmd += url
-                    click.echo('=====================================================')
+                if username and password:
+                    url = url.partition('://')
+                    url.insert(2, username + ':' + password + '@')
+                    url = ''.join(url)
+                elif username or password:
+                    click.echo(
+                        'Please enter both username and password if you wish do authentication for your requests')
+                if not os.path.isfile(urllib2.unquote(url.split('/')[-1])) or os.path.isfile(
+                                urllib2.unquote(url.split('/')[-1]) + '.st'):
+                    click.echo('======================================================================================')
                     click.echo(url)
-                    click.echo('=====================================================')
-                    execute(cmd)
+                    click.echo('======================================================================================')
+                    execute(cmd + '"' + url + '"')
+                    write_list.remove(url)
+                    file_generic.seek(0)
+                    file_generic.truncate()
+                    file_generic.flush()
+                    file_generic.writelines('\n'.join(write_list))
+                    file_generic.flush()
                 else:
-                    click.echo(urllib2.unquote(url.split('/')[-1]))
+                    click.echo('======================================================================================')
                     click.echo('The file "%s" exists' % unicode(urllib2.unquote(url.split('/')[-1])) +
                                ' in the downloading directory.\nif you are sure this is something else,' +
-                               ' please download it to somewrhere else or move the existing file')
-                write_list.remove(url)
-            file_generic.writelines(write_list)
-            file_generic.flush()
-            # TODO: return help in no arguments
+                               ' please download it to somewhere else or move the existing file')
+                    click.echo('======================================================================================')
+                # TODO: return help in no arguments
 
 
 def execute(cmd):
@@ -110,7 +130,7 @@ def execute(cmd):
         except KeyboardInterrupt:
             sys.stdout.flush()
             print ('\nInterrupted by user.\nyou can continue it later.')
-            exit()  # TODO: here before saving file application is closed
+            exit()
         if out == '' and proc.poll() is not None:
             print ('\nDownloaded the link successfully')
         if out != '' and out is str:
